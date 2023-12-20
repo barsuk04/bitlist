913fa932453064713d648a4d73317b3f73771a37
barsuk04 <107184377+barsuk04@users.noreply.github.com>
2023-12-20 23:59:04 +0500

    Delete bitlist directory

--- bitlist/scripts/burger.js
+++
@@ -1,8 +0,0 @@
-let headerBtn = document.querySelector(".header__burger");
-let headerMenu = document.querySelector(".burger");
-
-headerBtn.addEventListener("click", function () {
-    headerBtn.classList.toggle("active");
-    headerMenu.classList.toggle("active");
-    document.body.classList.toggle("_lock");
-});
